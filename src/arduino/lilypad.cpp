3428de7321b44972ab84e0ddaaab0a8877e78ff3
XervE <xerve.dev@gmail.com>
2026-02-06 11:51:47 +0100

    Updated folder structure

---
+++ src/arduino/lilypad.cpp
@@ -0,0 +1,9 @@
+void setup() {
+  // * Los de ASIR nos tienen que decir donde colocar todos los sensores, asi que estos pines deben cambiarse 
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

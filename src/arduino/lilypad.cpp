4e3400a71981bf78206ec9e853f9555811ce5fa4
XervE <xerve.dev@gmail.com>
2026-02-06 12:19:28 +0100

    Create lilypad main code

--- src/arduino/lilypad.cpp
+++ src/arduino/lilypad.cpp
@@ -1,3 +1,9 @@
+const int led = 0;
+const int vibrador = 1;
+const int luz = 2;
+const int infraRed = 3;
+const int peso = 4; // AQUI VA EL MODULO HX711, NO LA CELULA DE CARGA
+const int bt= 5; 
 void setup() {
   // * Los de ASIR nos tienen que decir donde colocar todos los sensores, asi que estos pines deben cambiarse 
 

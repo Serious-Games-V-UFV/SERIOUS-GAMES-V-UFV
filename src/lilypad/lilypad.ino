f3a8a8e54194961b420b042aca1245fdfe387716
XervE <xerve.dev@gmail.com>
2026-02-09 20:09:22 +0100

    Improvements in led control and timer
    Improvements on both features and add some commentarys for file structure
    ***
    Mejoras en ambas caracteristicas, añadido de comentarios para estructura del codigo

--- src/lilypad/lilypad.ino
+++ src/lilypad/lilypad.ino
@@ -11,9 +11,15 @@
     const int light = A2;
     
 
-// * Variables estáticas | Static variables
+// * Variables | variables
   static float bottleCapacity = 1.5;
   static float currentCapacity = 1.5;
+  static unsigned long clock = 0L;
+  static unsigned long lastActivity = millis();
+  static unsigned long lastDrink = 0L;
+  static float currentWeight = 0;
+  static float prevWeight = 1.5;
+  
 
 
 // * Configuracion de los pines bluetooth
@@ -25,27 +31,31 @@ HX711 scale;
 Adafruit_NeoPixel pixel(1, led, NEO_GRB + NEO_KHZ800);
 
 // * Metodos | Methods
+
+  // * Devuelve el peso medido en la bascula | Returns weight measured in the scale
   float findWeight(){
     float weight = scale.get_units(1);
     Serial.print(weight);
     return weight;
   }
 
+  // * Devuelve la cantidad de agua bebida o rellenada | Returns the water consumed or refilled
   float waterQuantity(float prevWeight, float weight){
     float total = 0;
     total = prevWeight - weight; // * El agua pesa 1g x cada ml (1Kg x cada litro) | Water weights 1g per ml (1kg per liter)
     return total;
   }
 
-  void ledControl(int drinkingTimer){
-    int hora = (drinkingTimer / 60);
-      if(hora<=1){
+  // * Controla el led en funcion de la hora de la ultima actividad | Controls the led in order of the last activity
+  void ledControl (long lastDrink){
+    long hours = lastDrink/3600000L;
+    if(hours<=1){
         pixel.setPixelColor(0, pixel.Color(0, 0, 0));
         pixel.show();
-      }else if((hora>1) && (hora<4)){
+      }else if((hours>1) && (hours<4)){
         pixel.setPixelColor(0, pixel.Color(255, 255, 0));
         pixel.show();
-      }else if((hora>=4) && (hora<6)){
+      }else if((hours>=4) && (hours<6)){
         pixel.setPixelColor(0, pixel.Color(255, 0, 0));
         pixel.show();
       }
@@ -59,11 +69,8 @@ Adafruit_NeoPixel pixel(1, led, NEO_GRB + NEO_KHZ800);
         pixel.setPixelColor(0, pixel.Color(255, 0, 0));
         pixel.show();
       }
-    }
+  }
   
-  
-  
-
 void setup() {
   pinMode(led,OUTPUT);
   pinMode(buzzer,OUTPUT);
@@ -75,33 +82,57 @@ void setup() {
   bt.begin(9600);
   Serial.begin(9600);
   pixel.begin();
-  // TODO Borrar esto cuando consigamos tarar bien la bascula | Delete this when we tare the scale 
+  
     // * Configuracion scale | Scale config
         scale.set_scale();
         scale.tare();
-        Serial.println("Pon peso conocido y lee Serial Monitor");
+        // TODO Borrar esto cuando consigamos tarar bien la bascula | Delete this when we tare the scale 
+          Serial.println("Pon peso conocido y lee Serial Monitor");
 }
 
 void loop() {
+  clock = millis();
+  lastDrink = (clock - lastActivity);
   //TODO Borrar esto cuando consigamos tarar bien la bascula | Delete this when we tare the scale 
     if (scale.is_ready()) {
           float reading = scale.get_units(10);  
           Serial.print("Lectura raw: ");
           Serial.println(reading);
         }
-
-  ledControl(drinkingTimer);
   
-  float currentWeight = findWeight();
-  float drunk = waterQuantity(prevWeight,currentWeight);
+  // * Control de los led por tiempo sin beber | Led control with time of last drink
+    ledControl(lastDrink);
 
-  if(drunk > 0.01){
-    currentCapacity -=drunk;
-  }else if(drunk <-0.1){
-    currentCapacity += fabs(drunk);
-  }
-  float prevWeight = currentWeight;
 
-  delay(2000);
+  // * Control de la vibración | Vibration control
+
+
+  
+  // * Agua bebida y modificacion capacidad actual de la botella | Drunk water and modify current bottle's capacity
+    currentWeight = findWeight();
+    float drunk = waterQuantity(prevWeight,currentWeight);
+    if(drunk > 0.01){
+      currentCapacity -=drunk;
+      lastActivity = clock;
+    }else if(drunk <-0.1){
+      currentCapacity += fabs(drunk); // * Si se rellena la capacidad aumenta fabs = valor absoluto | If there is a refill, capacity increases fabs = absolute value
+    }
+    prevWeight = currentWeight;
+  
+  // * Deteccion de la apertura de la botella | Bag opening detection
+
+
+
+
+
+  // * Deteccion del sensor infrarrojo | IR sensor detection
+
+
+  
+
+  // * Modo noche | Night mode
+  
+  
+  
+
 }
-

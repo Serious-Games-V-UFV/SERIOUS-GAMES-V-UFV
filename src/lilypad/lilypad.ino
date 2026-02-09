5263fa8397f5818fc5fa2757a2eef3924a3a4c2a
XervE <aviercerverarodriguez@gmail.com>
2026-02-09 12:20:52 +0100

    Update lilypad.ino

--- src/lilypad/lilypad.ino
+++ src/lilypad/lilypad.ino
@@ -1,16 +1,17 @@
 #include <SoftwareSerial.h>
 #include <HX711.h>
   // * Configuracion pines sensores | Component's pin config
-    const int led = 5;
+    const int led= 5;
     const int buzzer = 6;
-    const int light = A2;
     const int infraRed = 7;
     const int weightOut = 2;
     const int weightTrigger = 3;
+    const int light = A2;
 
 // * Variables estáticas | Static variables
   static int drinkingTimer = 0;
-  static float bottleCapacity = 1.5F;
+  static float bottleCapacity = 1.5;
+  static float currentCapacity = 1.5;
 
 // * Configuracion de los pines bluetooth
   const int pinRX = 8; // * Pin recepcion lilypad (Conecta al TX del modulo) | Reception pin lilypad (TX HC05)
@@ -18,18 +19,20 @@
 
 SoftwareSerial bt (pinRX, pinTx);
 HX711 scale;
+// * Metodos | Methods
+  float findWeight(HX711 scale){
+    float weight = scale.get_units(1);
+    Serial.print(weight);
+    return weight;
+  }
 
-float findWeight(HX711 scale){
-  float weight = scale.get_units(1);
-  Serial.print(weight);
-  return weight;
-}
+  float waterQuantity(float prevWeight, float weight){
+    float total = 0;
+    total = prevWeight - weight; // * El agua pesa 1g x cada ml (1Kg x cada litro) | Water weights 1g per ml (1kg per liter)
+      return total;
+  }
 
-float waterQuantity(float prevKg, float weight){
-  float total = 0F;
-  total = prevWeitgh - weight; // * El agua pesa 1g x cada ml (1Kg x cada litro) | Water weights 1g per ml (1kg per liter)
-    return total;
-}
+  
 
 void setup() {
   pinMode(led,OUTPUT);
@@ -41,21 +44,22 @@ void setup() {
   scale.begin(weightOut,weightTrigger);
   bt.begin(9600);
   Serial.begin(9600);
-// TODO Borrar esto cuando consigamos tarar bien la scale | Delete this when we tare the scale 
-  // * Configuracion scale | Scale config
-      scale.set_scale();
-      scale.tare();
-      Serial.println("Pon peso conocido y lee Serial Monitor");
-      
+  // TODO Borrar esto cuando consigamos tarar bien la bascula | Delete this when we tare the scale 
+    // * Configuracion scale | Scale config
+        scale.set_scale();
+        scale.tare();
+        Serial.println("Pon peso conocido y lee Serial Monitor");
 }
 
 void loop() {
-  //TODO Borrar esto cuando consigamos tarar bien la scale | Delete this when we tare the scale 
+  //TODO Borrar esto cuando consigamos tarar bien la bascula | Delete this when we tare the scale 
     if (scale.is_ready()) {
           float reading = scale.get_units(10);  
           Serial.print("Lectura raw: ");
           Serial.println(reading);
         }
+
+    // ledControl(drinkingTimer);
   
   float currentWeight = findWeight(scale);
   float prevWeight = currentWeight;
@@ -64,11 +68,12 @@ void loop() {
     do{
       currentWeight = findWeight(scale);
     }while(currentWeight <0);
-      float drinkenWater = waterQuantity(prevWeight,currentWeight);
-      if(drinkenWater < 0){
-        int capacidadBotella = (drinkenWater*-1)
+      float drinkenWater = waterQuantity(prevWeight,currentWeight); // * Se calcula el agua bebida | Drinken Water is calculated
+      if(drinkenWater < 0){ // * Si <0(Se ha rellenado la botella) actualizar capacidad | If <0 (bottle was refilled) update Capacity
+        int currentCapacity = currentCapacity + (drinkenWater*-1); 
       }
   }
+  drinkingTimer++;
   drinkingTimer++;
   delay(2000);
 }

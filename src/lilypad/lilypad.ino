9fd801fe9c119471827a7532b9e7944b48dfdb2f
XervE <aviercerverarodriguez@gmail.com>
2026-02-09 14:39:00 +0100

    Changes in lilypad.ino and added documentation

--- src/lilypad/lilypad.ino
+++ src/lilypad/lilypad.ino
@@ -1,17 +1,20 @@
 #include <SoftwareSerial.h>
 #include <HX711.h>
-  // * Configuracion pines sensores | Component's pin config
+#include <Adafruit_NeoPixel.h>
+#include <Math.h>
+// * Configuracion pines sensores | Component's pin config
     const int led= 5;
     const int buzzer = 6;
     const int infraRed = 7;
     const int weightOut = 2;
     const int weightTrigger = 3;
     const int light = A2;
+    
 
 // * Variables estáticas | Static variables
-  static int drinkingTimer = 0;
   static float bottleCapacity = 1.5;
   static float currentCapacity = 1.5;
+
 
 // * Configuracion de los pines bluetooth
   const int pinRX = 8; // * Pin recepcion lilypad (Conecta al TX del modulo) | Reception pin lilypad (TX HC05)
@@ -19,8 +22,10 @@
 
 SoftwareSerial bt (pinRX, pinTx);
 HX711 scale;
+Adafruit_NeoPixel pixel(1, led, NEO_GRB + NEO_KHZ800);
+
 // * Metodos | Methods
-  float findWeight(HX711 scale){
+  float findWeight(){
     float weight = scale.get_units(1);
     Serial.print(weight);
     return weight;
@@ -29,9 +34,34 @@ HX711 scale;
   float waterQuantity(float prevWeight, float weight){
     float total = 0;
     total = prevWeight - weight; // * El agua pesa 1g x cada ml (1Kg x cada litro) | Water weights 1g per ml (1kg per liter)
-      return total;
+    return total;
   }
 
+  void ledControl(int drinkingTimer){
+    int hora = (drinkingTimer / 60);
+      if(hora<=1){
+        pixel.setPixelColor(0, pixel.Color(0, 0, 0));
+        pixel.show();
+      }else if((hora>1) && (hora<4)){
+        pixel.setPixelColor(0, pixel.Color(255, 255, 0));
+        pixel.show();
+      }else if((hora>=4) && (hora<6)){
+        pixel.setPixelColor(0, pixel.Color(255, 0, 0));
+        pixel.show();
+      }
+      else{
+        pixel.setPixelColor(0, pixel.Color(255, 0, 0));
+        pixel.show();
+        delay(500);
+        pixel.setPixelColor(0, pixel.Color(255, 0, 0));
+        pixel.show();
+        delay(500);
+        pixel.setPixelColor(0, pixel.Color(255, 0, 0));
+        pixel.show();
+      }
+    }
+  
+  
   
 
 void setup() {
@@ -44,6 +74,7 @@ void setup() {
   scale.begin(weightOut,weightTrigger);
   bt.begin(9600);
   Serial.begin(9600);
+  pixel.begin();
   // TODO Borrar esto cuando consigamos tarar bien la bascula | Delete this when we tare the scale 
     // * Configuracion scale | Scale config
         scale.set_scale();
@@ -59,22 +90,18 @@ void loop() {
           Serial.println(reading);
         }
 
-    // ledControl(drinkingTimer);
+  ledControl(drinkingTimer);
   
-  float currentWeight = findWeight(scale);
-  float prevWeight = currentWeight;
-  currentWeight = findWeight(scale);
-  if(currentWeight < 0){
-    do{
-      currentWeight = findWeight(scale);
-    }while(currentWeight <0);
-      float drinkenWater = waterQuantity(prevWeight,currentWeight); // * Se calcula el agua bebida | Drinken Water is calculated
-      if(drinkenWater < 0){ // * Si <0(Se ha rellenado la botella) actualizar capacidad | If <0 (bottle was refilled) update Capacity
-        int currentCapacity = currentCapacity + (drinkenWater*-1); 
-      }
+  float currentWeight = findWeight();
+  float drunk = waterQuantity(prevWeight,currentWeight);
+
+  if(drunk > 0.01){
+    currentCapacity -=drunk;
+  }else if(drunk <-0.1){
+    currentCapacity += fabs(drunk);
   }
-  drinkingTimer++;
-  drinkingTimer++;
+  float prevWeight = currentWeight;
+
   delay(2000);
 }
 

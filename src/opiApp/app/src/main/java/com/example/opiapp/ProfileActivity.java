d419abdb672505d82b027956e8bb6e251b372c61
devxerve <xerve.dev@gmail.com>
2026-03-11 22:48:17 +0100

    added mysql connector

--- src/opiApp/app/src/main/java/com/example/opiapp/ProfileActivity.java
+++ src/opiApp/app/src/main/java/com/example/opiapp/ProfileActivity.java
@@ -9,8 +9,6 @@ public class ProfileActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
-
         setupBottomNavigation();
     }
 }
